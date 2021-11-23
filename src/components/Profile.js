4fd4eee0ab019f768051ce1f42227188a5af7b41
mgacoder <miguelgalvanalvarado@gmail.com>
2021-11-22 22:13:31 -0800

    profile updated

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -7,6 +7,10 @@ import { logoutUser } from '../redux/actions/userActions';
 const Profile = () => {
     const dispatch = useDispatch();
     const image = useSelector(state => state.userReducer.profileImage);
+    const nickname = useSelector(state => state.userReducer.nickname);
+    const description = useSelector(state => state.userReducer.aboutMe);
+    const activities = useSelector(state => state.userReducer.activities);
+    
     console.log('This is the image ', image);
     const navigate = useNavigate();
     const handleClick = (route) => {
@@ -40,6 +44,11 @@ const Profile = () => {
            
             
             <img src={URL.createObjectURL(image)} width={200} height={200}/>
+            <h3>{nickname}</h3>
+            <h5>{description}</h5>
+            <h5>Activities</h5>
+            {activities.map(activity => <h6>{activity}</h6>)}
+            
             <div>
                 <button onClick={() => handleClick('calendar')}>Calendar</button>
                 <button onClick={() => handleClick('workouts')}>Workouts</button>
