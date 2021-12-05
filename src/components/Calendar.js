a37a0a37286410b3473ff6c12bda136e362869f8
mgacoder <miguelgalvanalvarado@gmail.com>
2021-12-04 22:25:41 -0800

    commiting

--- src/components/Calendar.js
+++ src/components/Calendar.js
@@ -3,8 +3,6 @@ import Calendar from 'react-calendar';
 import 'react-calendar/dist/Calendar.css';
 import './cal.css';
 
-
-
 const CalendarApp = () => {
 const [value, onChange] = useState(new Date());
 
