4ddbe3c2fb08ca220c867950a84327bb540dd1d2
jasjahan <mjahan@mail.sfsu.edu>
2021-12-06 12:15:19 -0800

    Update on Search Page
    Major Change

--- src/components/Search.js
+++ src/components/Search.js
@@ -20,8 +20,11 @@ const Search = () => {
         case "login":
           navigate("/login");
           return;
+        case "search":
+          navigate("/search");
+          return; 
         case "register":
-          navigate("register");
+          navigate("/register");
           return;
         default:
           return;
@@ -51,6 +54,30 @@ const Search = () => {
                     </ul>
                 </div>
       </nav>
+      <div>
+      <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css"
+                        integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous"/>
+            <div className="container">
+              <div class="thumbnail">
+                    <div className="row justify-content-center">
+                      <div className="col-12 col-md-10 col-lg-8 col-sm-4 search-bar">
+                         <form className="card card-sm" >
+                            <div className="cardbody row no-gutters align-items-center">        
+                              <div className="col">
+                                 <input className="form-control form-control-lg "
+                                    type="search" placeholder="What are you waiting for?" ></input>
+                              </div>
+                             <div className="col-auto"> 
+                               <button className="btn btn-lg btn-primary" type="button"onClick={() => handleClick("search")}>Search</button> 
+                              </div>
+                            </div>
+                         </form>
+                      </div> 
+                   </div>
+               </div>
+                   <br/>
+             </div>
+      </div>
       <div class="child">  </div>
       <h4 className="" align = "center"> Search Results </h4> 
       <div class="childhome">  </div>
@@ -60,7 +87,7 @@ const Search = () => {
             <div class="row no-gutters">
                 <div class="col-md-4">
                     <a href="" class="btn btn-fix text-left">
-                    <img  src ={foo4x} class="card-img" alt="Broken"/></a>
+                    <img  src ={foo4} class="card-img" alt="Broken"/></a>
                 </div>
 
                 <div class="col-md-8">
